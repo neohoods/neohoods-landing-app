38cfa8179b2bba5d23cf5429d30510e21b7ef56f
Quentin Castel <quentin.castel@dataiku.com>
2025-09-13 16:47:23 +0200

    fix ci

---
+++ commitlint.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // Nouvelle fonctionnalité
+        'fix',      // Correction de bug
+        'docs',     // Documentation
+        'style',    // Formatage, point-virgules manquants, etc.
+        'refactor', // Refactoring du code
+        'perf',     // Amélioration des performances
+        'test',     // Ajout de tests
+        'chore',    // Tâches de maintenance
+        'ci',       // Configuration CI/CD
+        'build',    // Build system
+        'revert'    // Annulation d'un commit
+      ]
+    ],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never']
+  }
+};
